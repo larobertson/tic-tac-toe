25add4ca0e2da33f11d39af441ba8e79c2d570c0
Laura <laura.robertson64@gmail.com>
2019-01-28 15:36:01 -0600

    should render players to DOM by turn

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
+let gameField = [[null, null, null], [null, null, null], [null, null, null]]
+
+//each turn, starting with player x
+//turn on the game position, 1 for player x, 0 for player 0
+//if one line is complete, player wins
+  //entire column
+  //entire row
+  //diagonal
+//if position !null, then it cannot be played
+
 
 let turn = true;
-//decide who's turn it is
-//after each turn flip the turn boolean
-if(turn) {
-    let xPlay = (elementId) => {
-        //here I need to figure out how to get ANY clicked ID
-    document.getElementById(elementId).innerHTML = "X";
+let player = "X";
+
+//make a list of clicked elements:
+let clicked = [];
+
+let renderToDom = (elementId) => {
+  if(clicked.includes(elementId)) {
+    alert('that space is taken')
+  } else {
+    clicked.push(elementId);
+    if(turn) {
+        player = "X"
+      } else {
+        player = "O";
+      }
+    document.getElementById(elementId).innerHTML = player;
     turn = !turn;
-    }
-} else {
-    let oPlay = (elementId) => {
-        //here I need to figure out how to get ANY clicked ID
-    document.getElementById(elementId).innerHTML = "O";
-    turn = !turn;
-    }
+    //still need to map to game array
+  }
 }
 
+
 //somewhere down here I want a reset game button
+let reset = document.getElementById("reset")
+reset.addEventListener("click", () => {
+    location.reload();
+  });
 
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
@@ -24,6 +44,6 @@ if(turn) {
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 
-document.addEventListener("click", () => {
-    document.getElementById("r1c1").innerHTML = "X";
-  });
+// document.addEventListener("click", () => {
+//     document.getElementById("r1c1").innerHTML = "X";
+//   });
