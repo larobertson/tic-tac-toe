41568301cc0b909bd16d3286560d312eeed5a453
Laura <laura.robertson64@gmail.com>
2019-01-28 20:18:47 -0600

    working tic-tac-toe with reset button

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let turn = true;
 let player = "X";
 
 let gameField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+let xWins = 0;
+let oWins = 0;
 
 let mapToField = (player, id) => {
   let row = Number(id[1]) - 1;
@@ -36,43 +38,52 @@ let renderToDom = (elementId) => {
 ///////////////////////////////////////////////////////////////////
 
 let checkRows = (row) => {
-    console.log(row[0] === 'X', row[1] === 'X', row[2] === 'X');
     if(row[0] === 'X' && row[1] === 'X' && row[2] === 'X') {
          alert('X Wins!');
-         location.reload()
+         xWins += 1;
     } else if (row[0] === 'O' && row[1] === 'O' && row[2] === 'O'){
         alert('O Wins!');
-        location.reload();
+        oWins += 1;
     }
 }
 
 let checkColumn = (gameField) => {
   if(gameField[0][0] === 'X' && gameField[1][0] === 'X' && gameField[2][0] === 'X') {
       alert('X Wins!');
+      xWins += 1;
   } else if (gameField[0][1] === 'X' && gameField[1][1] === 'X' && gameField[2][1] === 'X') {
       alert('X Wins!')
+      xWins += 1;
   } else if (gameField[0][2] === 'X' && gameField[1][2] === 'X' && gameField[2][2] === 'X') {
     alert('X Wins!')
+    xWins += 1;
   }
   if(gameField[0][0] === 'O' && gameField[1][0] === 'O' && gameField[2][0] === 'O') {
     alert('O Wins!');
+    oWins += 1;
   } else if (gameField[0][1] === 'O' && gameField[1][1] === 'O' && gameField[2][1] === 'O') {
-    alert('O Wins!')
+    alert('O Wins!');
+    oWins += 1;
   } else if (gameField[0][2] === 'O' && gameField[1][2] === 'O' && gameField[2][2] === 'O') {
-    alert('O Wins!')
+    alert('O Wins!');
+    oWins += 1;
   }
 }
 
 let checkDiagonal = (gameField) => {
     if(gameField[0][0] === 'X' && gameField[1][1] === 'X' && gameField[2][2] === 'X') {
         alert('X Wins!');
+        xWins += 1;
     } else if (gameField[2][0] === 'X' && gameField[1][1] === 'X' && gameField[0][2] === 'X') {
-        alert('X Wins!')
+        alert('X Wins!');
+        xWins += 1;
     }
     if(gameField[0][0] === 'O' && gameField[1][1] === 'O' && gameField[2][2] === 'O') {
       alert('O Wins!');
+      oWins += 1;
     } else if (gameField[2][0] === 'O' && gameField[1][1] === 'O' && gameField[0][2] === 'O') {
-      alert('O Wins!')
+      alert('O Wins!');
+      oWins += 1;
     } 
   }
 
@@ -87,8 +98,17 @@ let gameStatus = (gameField) => {
 
 let reset = document.getElementById("reset")
 reset.addEventListener("click", () => {
-    location.reload();
+    clicked.forEach((value) => {
+        document.getElementById(value).innerHTML = '';
+    });
+    clicked = [];
+    gameField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   });
+
+
+document.getElementById("xScore").innerHTML = xWins.toString();
+document.getElementById("oScore").innerHTML = oWins.toString();
+
 
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
